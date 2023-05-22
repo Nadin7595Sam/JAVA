9076a8c8dce9d48e86065ec35080aa0120c73d8e
Nadin <nadyusha-serdyuk@inbox.ru>
2023-05-22 21:20:18 +0300

    done

--- HW-6/task6_1.java
+++ HW-6/task6_1.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 class Laptop {
-    private String model;  // Модель ноутбука
-    private int storageCapacity;  // Объем памяти ноутбука
-    private String color;  // Цвет ноутбука
-    private int price;  // Цена ноутбука
+    String model;
+    int storageCapacity;
+    String color;
+    int price;
 
     public Laptop(String model, int storageCapacity, String color, int price) {
         this.model = model;
@@ -12,69 +12,71 @@ class Laptop {
         this.color = color;
         this.price = price;
     }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getStorageCapacity() {
-        return storageCapacity;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getPrice() {
-        return price;
-    }
 }
 
 public class task6_1 {
     public static void main(String[] args) {
         // Создаем множество ноутбуков
         Set<Laptop> laptops = new HashSet<>();
-        laptops.add(new Laptop("Dell Inspiron", 256, "Чёрный", 50000));
-        laptops.add(new Laptop("HP Pavilion", 512, "Серый", 60000));
-        laptops.add(new Laptop("Lenovo ThinkPad", 512, "Чёрный", 55000));
+        laptops.add(new Laptop("Huawei", 256, "Black", 50000));
+        laptops.add(new Laptop("HP", 512, "Silver", 60000));
+        laptops.add(new Laptop("Lenovo", 512, "Black", 55000));
 
-        // Создаем объект Scanner для чтения ввода пользователя
+        // Запрашиваем у пользователя критерии фильтрации
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите цифру критерия:");
+        System.out.println("1 - Объем ЖД");
+        System.out.println("2 - Цена");
+        System.out.println("3 - Цвет");
+        int criteria = scanner.nextInt();
 
-        // Создаем Map критериев фильтрации
-        Map<Integer, String> criteria = new HashMap<>();
-        criteria.put(1, "Объем памяти");  // Объем памяти
-        criteria.put(2, "Цена");  // Цена
-        criteria.put(3, "Цвет");  // Цвет
+        // Запрашиваем минимальное значение выбранного критерия
+        System.out.print("Введите минимальное значение этого критерия: ");
+        int minValue = scanner.nextInt();
 
-        // Создаем Map параметров фильтрации
-        Map<Integer, Object> filters = new HashMap<>();
+        // Инициализируем переменную для отфильтрованного ноутбука
+        Laptop filteredLaptop = null;
 
-        // Запрашиваем у пользователя критерии и минимальные значения фильтрации
-        for (int i = 1; i <= criteria.size(); i++) {
-            System.out.print("Введите минимальное значение для " + criteria.get(i) + ": ");
-            filters.put(i, scanner.nextLine());
+        // Фильтруем ноутбуки и сохраняем прошедший фильтр
+        for (Laptop laptop : laptops) {
+            boolean passFilter = false;
+
+            // Фильтруем по выбранному критерию
+            switch (criteria) {
+                case 1:
+                    if (laptop.storageCapacity >= minValue) {
+                        passFilter = true;
+                    }
+                    break;
+                case 2:
+                    if (laptop.price >= minValue) {
+                        passFilter = true;
+                    }
+                    break;
+                case 3:
+                    if (laptop.color.equalsIgnoreCase(String.valueOf(minValue))) {
+                        passFilter = true;
+                    }
+                    break;
+                default:
+                    System.out.println("Некорректный критерий!");
+            }
+
+            // Сохраняем прошедший фильтр ноутбук
+            if (passFilter) {
+                filteredLaptop = laptop;
+                break;
+            }
         }
 
-        // Фильтруем ноутбуки и выводим результаты
-        for (Laptop laptop : laptops) {
-            boolean meetsCriteria = true;
-            for (int i = 1; i <= criteria.size(); i++) {
-                Object value = filters.get(i);
-                if (i == 1 && laptop.getStorageCapacity() < Integer.parseInt((String) value)) {
-                    meetsCriteria = false;
-                    break;
-                } else if (i == 2 && laptop.getPrice() > Integer.parseInt((String) value)) {
-                    meetsCriteria = false;
-                    break;
-                } else if (i == 3 && !laptop.getColor().equalsIgnoreCase((String) value)) {
-                    meetsCriteria = false;
-                    break;
-                }
-            }
-            if (meetsCriteria) {
-                System.out.println(laptop.getModel());
-            }
+        // Выводим информацию о подходящем ноутбуке
+        if (filteredLaptop != null) {
+            System.out.println("Модель: " + filteredLaptop.model);
+            System.out.println("Объем ЖД: " + filteredLaptop.storageCapacity);
+            System.out.println("Цвет: " + filteredLaptop.color);
+            System.out.println("Цена: " + filteredLaptop.price);
+        } else {
+            System.out.println("Такой ноутбук не найден.");
         }
     }
 }
